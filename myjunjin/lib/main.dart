46184d08f403d0c5f84a0e069b007c59e2e6d309
du <duyux@163.com>
2018-12-11 11:36:06 +0800

    Update main.dart

--- myjunjin/lib/main.dart
+++ myjunjin/lib/main.dart
@@ -18,58 +18,9 @@ class MyApp extends StatelessWidget {
         iconTheme: new IconThemeData(size:18.0),//设置icon样式
         primaryTextTheme: new TextTheme(title: new TextStyle(color:Colors.black,fontSize: 16.0)),//设置文本样式
       ),
-      //home: MyHomePage(title: 'Flutter Demo Home Page'),
        home: IndexPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-   
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-  
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
